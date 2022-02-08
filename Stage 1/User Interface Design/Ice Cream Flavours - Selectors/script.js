d8686f34eaf1217a46324bbca2f3834e90eaf8f8
Ramkumar <64207937+ramkumarm15@users.noreply.github.com>
2022-02-08 17:23:12 +0530

    Create script.js

---
+++ Stage 1/User Interface Design/Ice Cream Flavours - Selectors/script.js
@@ -0,0 +1,7 @@
+$("p").css("color", "orange");
+$('.flavours li').css({
+    'background-color': 'lightBlue',
+    'font-size': '120%'
+});
+$('p:nth-of-type(2)').css('font-size', '50%');
+$('#icecreamfloats li').css('background-color', 'orange');
